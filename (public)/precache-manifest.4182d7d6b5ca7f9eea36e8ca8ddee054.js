1477d76a3fa365bada3905f2bb1ebe147f65354c
joshua Irving B. Fabricante <joshua.starkiller115@gmail.com>
2021-08-30 20:02:34 +0800

    changes build files

---
+++ (public)/precache-manifest.4182d7d6b5ca7f9eea36e8ca8ddee054.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f821c69440c1e0ea73e665c580857964",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa072a04374c8c51ecaf",
+    "url": "/static/css/2.9f12f3b0.chunk.css"
+  },
+  {
+    "revision": "7d8d3c6021f98b26d995",
+    "url": "/static/css/main.2eeae061.chunk.css"
+  },
+  {
+    "revision": "aa072a04374c8c51ecaf",
+    "url": "/static/js/2.aa49ae35.chunk.js"
+  },
+  {
+    "revision": "c346446ec754038f90188f8f970f9c39",
+    "url": "/static/js/2.aa49ae35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d8d3c6021f98b26d995",
+    "url": "/static/js/main.eaea62d3.chunk.js"
+  },
+  {
+    "revision": "1ca56b6e53ce1dc83ae6",
+    "url": "/static/js/runtime-main.0a3c20bd.js"
+  },
+  {
+    "revision": "902c19dbae678c578b0ad36b09d902b8",
+    "url": "/static/media/BigStitchy-logo.902c19db.png"
+  },
+  {
+    "revision": "58a6e78e75d8112f261488b5d6f812be",
+    "url": "/static/media/Facilities.58a6e78e.png"
+  },
+  {
+    "revision": "de8128a956367ee09bc6af6b3b5629af",
+    "url": "/static/media/covid19.de8128a9.jpg"
+  },
+  {
+    "revision": "f40a462e93ac9c02dc56466c02b24689",
+    "url": "/static/media/github-app.f40a462e.png"
+  },
+  {
+    "revision": "e1afbbf9126f6b42fa0eb46b52b9d5dc",
+    "url": "/static/media/gustavo.e1afbbf9.png"
+  },
+  {
+    "revision": "38f1589db684169e6e680cc0f300cf71",
+    "url": "/static/media/profile_pic.38f1589d.jpg"
+  },
+  {
+    "revision": "e2bd3519631c4429139057a16b97fff9",
+    "url": "/static/media/tidyschools.e2bd3519.png"
+  },
+  {
+    "revision": "bee31c7a2b6bf51669206588341af61f",
+    "url": "/static/media/veronica.bee31c7a.png"
+  },
+  {
+    "revision": "4a6faabd20a42e875c23875d11ad6c76",
+    "url": "/static/media/wrupup-2.4a6faabd.jpg"
+  },
+  {
+    "revision": "fd5f3beeac7fd1757834e6bd989615d8",
+    "url": "/static/media/wrupup.fd5f3bee.jpg"
+  }
+]);
